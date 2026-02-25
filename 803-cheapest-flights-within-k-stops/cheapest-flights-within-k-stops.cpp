fd346d0c0023069b25ed500b59a188bba088f6ce
Anshul Fauzdar <anshulfauzdar2415@gmail.com>
2026-02-25 10:39:03 +0530

    Time: 7 ms (29.85%) | Memory: 17 MB (98.84%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-
-        vector<vector<pair<int,int>>> adj(n);
-
-        for(auto flight : flights){
-            int from = flight[0];
-            int to = flight[1];
-            int price = flight[2];
-            adj[from].push_back({to, price});
-        }
-
-        vector<int> dist(n, INT_MAX);
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        const int INF = 1e9;
+        vector<int> dist(n, INF);
         dist[src] = 0;
 
-        queue<pair<int, pair<int,int>>> q;
-        q.push({0, {0, src}});
-
-        while(!q.empty()){
-            auto front = q.front();
-            q.pop();
-
-            int stops = front.first;
-            int currCost = front.second.first;
-            int node = front.second.second;
-
-            for(auto neighbourData : adj[node]){
-                int nextNode = neighbourData.first;
-                int edgeCost = neighbourData.second;
-
-                if((currCost + edgeCost < dist[nextNode]) && (stops <= k)){
-                    dist[nextNode] = currCost + edgeCost;
-                    q.push({stops + 1, {dist[nextNode], nextNode}});
+        for(int i = 0; i <= K; i++){
+            vector<int> temp = dist;   // important
+            for(auto &e : flights){
+                int u = e[0], v = e[1], w = e[2];
+                if(dist[u] != INF && dist[u] + w < temp[v]){
+                    temp[v] = dist[u] + w;
                 }
             }
+            dist = temp;
         }
-
-        if(dist[dst] == INT_MAX) return -1;
-        return dist[dst];
+        return dist[dst] == INF ? -1 : dist[dst];
     }
 };
