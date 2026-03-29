acba7b1fa331024d981307943dfef7902e15d23d
Anshul Fauzdar <anshulfauzdar2415@gmail.com>
2026-03-29 19:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (76.97%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dp(n, 0);
+
+        dp[0] = grid[0][0];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+
+                if(i == 0)
+                    dp[j] = dp[j-1] + grid[i][j];
+                else if(j == 0)
+                    dp[j] = dp[j] + grid[i][j];
+                else
+                    dp[j] = min(dp[j], dp[j-1]) + grid[i][j];
+            }
+        }
+
+        return dp[n-1];
+    }
+};
