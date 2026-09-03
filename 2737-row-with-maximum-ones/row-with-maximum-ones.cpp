f864569b1b0153933292deb51bf614f1398dec9f
Anshul Fauzdar <anshulfauzdar2415@gmail.com>
2026-09-03 16:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 64.6 MB (69.61%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int maxCount = 0, rowIndex = 0;
-        for (int i = 0; i < mat.size(); ++i) {
-            int count = 0;
-            for (int val : mat[i]) {
-                count += val;
+        // int maxCount = 0, rowIndex = 0;
+        // for (int i = 0; i < mat.size(); ++i) {
+        //     int count = 0;
+        //     for (int val : mat[i]) {
+        //         count += val;
+        //     }
+        //     if (count > maxCount) {
+        //         maxCount = count;
+        //         rowIndex = i;
+        //     }
+        // }
+        // return {rowIndex, maxCount};
+        int maxCount = 0 ;
+        int rowIndex = 0 ;
+        for(int i = 0 ; i<mat.size() ; i++){
+            int count = 0 ;
+            for(int val : mat[i]){
+                count += val ;
             }
-            if (count > maxCount) {
+            if(count > maxCount){
                 maxCount = count;
                 rowIndex = i;
             }
         }
-        return {rowIndex, maxCount};
+        return{rowIndex , maxCount};
     }
 };
